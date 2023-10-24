43c8d2e6d007f7307dc9404f25c980e71513d25c
Kacper Nalepa <kacper.nlp@wp.pl>
2023-10-24 18:47:45 +0200

    Created AppNavLink component

---
+++ components/AppNavLink.vue
@@ -0,0 +1,22 @@
+<template>
+    <nuxt-link :to="link" class="text-sm text-zinc-500 hover:text-lime-600 duration-300"><slot></slot></nuxt-link>
+</template>
+
+<style>
+.nuxt-link-exact-active {
+    color: rgba(101, 163, 13);
+}
+</style>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        link: {
+            type: String,
+            required: true,
+        },
+    },
+})
+</script>
