27772f6950aa6dc9514d5c94c1c2a88c6b236822
Kacper Nalepa <kacper.nlp@wp.pl>
2023-11-07 21:01:54 +0100

    install Vue Class Components dependencies

---
+++ README.md
@@ -0,0 +1,1 @@
+Projekt w trakcie refaktoryzacji i migracji.
--- pages/product/_id.vue
+++ pages/product/_id.vue
@@ -3,3 +3,13 @@
         <h1>PDP</h1>
     </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    mounted() {
+        console.log(this.$route)
+    },
+})
+</script>
