276b740084a364228aaa24f546fb4cf6fe32763d
KacperNlp <kacper.nlp@wp.pl>
2024-04-21 13:03:35 +0200

    Create basic aside navigation for account settings

---
+++ client/components/AppAccountSideNav.vue
@@ -0,0 +1,26 @@
+<template>
+    <aside class="w-full max-w-64">
+        <el-card class="md:py-4">
+            <ul class="flex flex-col gap-2">
+                <li v-for="{ link, title, id } in ASIDE_NAV" :key="id">
+                    <NuxtLink :to="link" class="hover:text-blue-500">{{ title }}</NuxtLink>
+                </li>
+            </ul>
+        </el-card>
+    </aside>
+</template>
+
+<script setup lang="ts">
+const ASIDE_NAV = [
+    {
+        link: '/account',
+        title: 'User Account',
+        id: 1
+    },
+    {
+        link: '/wishlist',
+        title: 'Wishlist',
+        id: 2
+    }
+]
+</script>
--- client/pages/account.vue
+++ client/pages/account.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
+  <AppSectionBox class="flex flex-col md:flex-row gap-4 lg:gap-8 xl:gap-16">
+    <AppAccountSideNav />
     <h1>Acount Page</h1>
-  </div>
+  </AppSectionBox>
 </template>
+
+<script setup lang="ts">
+const ASIDE_NAV = [
+  {
+    link: '/account',
+    title: 'User Account',
+    id: 1
+  },
+  {
+    link: '/wishlist',
+    title: 'Wishlist',
+    id: 2
+  }
+]
+</script>
