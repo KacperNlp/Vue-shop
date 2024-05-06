5a4d502a0ce0a5bccce158df032f3fddd8dec8f5
Kacper Nalepa <kacper.nlp@wp.pl>
2024-05-06 20:02:04 +0200

    Add basic validation for register and login. After successful login/register redirect to account view

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -1,43 +1,65 @@
 <template>
-    <AppSectionBox>
-        <el-card class="flex flex-col items-center max-w-lg mx-auto py-2 pb-4">
-            <AppHeadline :headlineType="HeadlinesTypes.H2" class="text-center">Login</AppHeadline>
-            <form @submit.prevent="handleSubmitLoginForm" class="flex flex-col gap-2">
-                <el-form-item label=" Login:" class="form-input-box">
-                    <el-input v-model="userLoginData.userName" required />
-                </el-form-item>
-                <el-form-item label="Password:" class="form-input-box">
-                    <el-input v-model="userLoginData.password" type="password" required />
-                </el-form-item>
-                <el-button native-type="submit" type="success" class="mt-4">Login</el-button>
-            </form>
-            <div class="mt-4 text-sm text-center">
-                <span>You don't have account? Then <NuxtLink to="/register" class="text-green-500 hover:text-green-400">
-                        Register
-                    </NuxtLink></span>
-            </div>
-        </el-card>
-    </AppSectionBox>
+  <AppSectionBox>
+    <el-card class="flex flex-col items-center max-w-lg mx-auto py-2 pb-4">
+      <AppHeadline :headlineType="HeadlinesTypes.H2" class="text-center"
+        >Login</AppHeadline
+      >
+      <form @submit.prevent="handleSubmitLoginForm" class="flex flex-col gap-2">
+        <el-form-item label=" Login:" class="form-input-box">
+          <el-input v-model="userLoginData.userName" required />
+        </el-form-item>
+        <el-form-item label="Password:" class="form-input-box">
+          <el-input v-model="userLoginData.password" type="password" required />
+        </el-form-item>
+        <el-button native-type="submit" type="success" class="mt-4"
+          >Login</el-button
+        >
+      </form>
+      <div class="mt-4 text-sm text-center">
+        <span
+          >You don't have account? Then
+          <NuxtLink to="/register" class="text-green-500 hover:text-green-400">
+            Register
+          </NuxtLink></span
+        >
+      </div>
+    </el-card>
+  </AppSectionBox>
 </template>
 
 <script setup lang="ts">
-import { HeadlinesTypes } from '@/enums/enums';
-import type { UserLogin } from '@/types/types';
+import { HeadlinesTypes } from "@/enums/enums";
+import type { UserLogin } from "@/types/types";
+
+const router = useRouter();
+const config = useRuntimeConfig();
 
 const userLoginData = reactive<UserLogin>({
-    userName: '',
-    password: ''
-})
+  userName: "",
+  password: "",
+});
 
 async function handleSubmitLoginForm() {
-    try {
-        await useAPIFetch("/auth/local");
-    } catch (err) {
-        ElNotification({
-            title: 'Error',
-            message: 'Sorry, you cannot login, please try again later!',
-            type: 'error',
-        })
+  try {
+    const res = await useFetch(`${config.public.baseURL}/auth/local`, {
+      method: "POST",
+      body: {
+        password: userLoginData.password,
+        identifier: userLoginData.userName,
+      },
+    });
+
+    if (!!res.error.value) {
+      throw new Error(res.error.value.data.error.message);
     }
+
+    router.push({ path: "/account" });
+  } catch (err) {
+    ElNotification({
+      title: "Error",
+      message: err as string,
+      type: "error",
+    });
+  }
 }
 </script>
--- client/pages/register.vue
+++ client/pages/register.vue
@@ -1,79 +1,98 @@
 <template>
-    <AppSectionBox>
-        <el-card class="flex flex-col items-center max-w-xl mx-auto py-2 pb-4">
-            <AppHeadline :headlineType="HeadlinesTypes.H2" class="text-center">Register</AppHeadline>
-            <form @submit.prevent="handleSubmitRegisterForm" class="flex flex-col gap-2">
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-                    <el-form-item label=" Login:" class="form-input-box">
-                        <el-input v-model="userLoginData.userName" required />
-                    </el-form-item>
-                    <el-form-item label="E-mail:" class="form-input-box">
-                        <el-input v-model="userLoginData.email" type="email" required />
-                    </el-form-item>
-                    <el-form-item label="Password:" class="form-input-box">
-                        <el-input v-model="userLoginData.password" type="password" required />
-                    </el-form-item>
-                    <el-form-item label="Repeat password:" class="form-input-box">
-                        <el-input v-model="userLoginData.passwordRepeat" type="password" required />
-                    </el-form-item>
-                    <el-form-item label="Name:" class="form-input-box">
-                        <el-input v-model="userLoginData.name" required />
-                    </el-form-item>
-                    <el-form-item label="Surname:" class="form-input-box">
-                        <el-input v-model="userLoginData.suername" required />
-                    </el-form-item>
-                </div>
-                <el-button native-type="submit" type="success" class="mt-4">Register</el-button>
-            </form>
-            <div class="mt-4 text-sm text-center">
-                <span>
-                    You have already account? Then <NuxtLink to="/login" class="text-green-500 hover:text-green-400">
-                        Login</NuxtLink>
-                </span>
-            </div>
-        </el-card>
-    </AppSectionBox>
+  <AppSectionBox>
+    <el-card class="flex flex-col items-center max-w-xl mx-auto py-2 pb-4">
+      <AppHeadline :headlineType="HeadlinesTypes.H2" class="text-center"
+        >Register</AppHeadline
+      >
+      <form
+        @submit.prevent="handleSubmitRegisterForm"
+        class="flex flex-col gap-2"
+      >
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <el-form-item label=" Login:" class="form-input-box">
+            <el-input v-model="userLoginData.userName" required />
+          </el-form-item>
+          <el-form-item label="E-mail:" class="form-input-box">
+            <el-input v-model="userLoginData.email" type="email" required />
+          </el-form-item>
+          <el-form-item label="Password:" class="form-input-box">
+            <el-input
+              v-model="userLoginData.password"
+              type="password"
+              required
+            />
+          </el-form-item>
+          <el-form-item label="Repeat password:" class="form-input-box">
+            <el-input
+              v-model="userLoginData.passwordRepeat"
+              type="password"
+              required
+            />
+          </el-form-item>
+          <el-form-item label="Name:" class="form-input-box">
+            <el-input v-model="userLoginData.name" required />
+          </el-form-item>
+          <el-form-item label="Surname:" class="form-input-box">
+            <el-input v-model="userLoginData.suername" required />
+          </el-form-item>
+        </div>
+        <el-button native-type="submit" type="success" class="mt-4"
+          >Register</el-button
+        >
+      </form>
+      <div class="mt-4 text-sm text-center">
+        <span>
+          You have already account? Then
+          <NuxtLink to="/login" class="text-green-500 hover:text-green-400">
+            Login</NuxtLink
+          >
+        </span>
+      </div>
+    </el-card>
+  </AppSectionBox>
 </template>
 
 <script setup lang="ts">
-import { HeadlinesTypes } from '@/enums/enums';
-import type { UserRegister } from '@/types/types';
+import { HeadlinesTypes } from "@/enums/enums";
+import type { UserRegister } from "@/types/types";
 
 const config = useRuntimeConfig();
-const route = useRoute();
+const router = useRouter();
 
 const userLoginData = reactive<UserRegister>({
-    userName: '',
-    password: '',
-    passwordRepeat: '',
-    name: '',
-    surname: '',
-    email: ''
-})
+  userName: "",
+  password: "",
+  passwordRepeat: "",
+  name: "",
+  surname: "",
+  email: "",
+});
 
 async function handleSubmitRegisterForm() {
-    try {
-        const { userName, password, name, email } = userLoginData;
+  try {
+    const { userName, password, name, email } = userLoginData;
 
-        const response = await useFetch(`${config.public.baseURL}/auth/local/register`,
-            {
-                method: 'POST',
-                body: {
-                    name: name,
-                    password: password,
-                    email: email,
-                    username: userName
-                }
-            }
-        );
+    const res = await useFetch(`${config.public.baseURL}/auth/local/register`, {
+      method: "POST",
+      body: {
+        name: name,
+        password: password,
+        email: email,
+        username: userName,
+      },
+    });
 
-        route.push({ path: '/account' });
-    } catch (err: Error) {
-        ElNotification({
-            title: 'Error',
-            message: 'Error',
-            type: 'error',
-        })
+    if (!!res.error.value) {
+      throw new Error(res.error.value.data.error.message);
     }
+
+    router.push({ path: "/account" });
+  } catch (err) {
+    ElNotification({
+      title: "Error",
+      message: err as string,
+      type: "error",
+    });
+  }
 }
 </script>
