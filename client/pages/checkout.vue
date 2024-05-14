4a81d6558199013c71543ebe0361ecde20a2965c
Kacper Nalepa <kacper.nlp@wp.pl>
2024-05-14 19:24:44 +0200

    Build checkout form with additional data like shipping method, payment option and order note

--- client/pages/checkout.vue
+++ client/pages/checkout.vue
@@ -2,40 +2,102 @@
   <AppSectionBox class="flex flex-col gap-4 md:grid md:grid-cols-[2fr_1fr]">
     <div>
       <AppHeadline :headlineType="HeadlinesTypes.H1">Checkout</AppHeadline>
-      <span v-if="!isLoggedIn" class="text-sm text-gray-600">Already have an account?
-        <NuxtLink to="/login">Log in</NuxtLink>.
+      <span v-if="!isLoggedIn" class="text-sm text-gray-600"
+        >Already have an account? <NuxtLink to="/login">Log in</NuxtLink>.
       </span>
-      <form action="POST" @submit.prevent="handleSubmitFinishOrder" class="max-w-2xl mt-4">
+      <form
+        action="POST"
+        @submit.prevent="handleSubmitFinishOrder"
+        class="max-w-2xl mt-4"
+      >
         <section class="my-6">
-          <AppHeadline :headlineType="HeadlinesTypes.H3">Your data:</AppHeadline>
+          <AppHeadline :headlineType="HeadlinesTypes.H3"
+            >Your data:</AppHeadline
+          >
           <div class="flex flex-col gap-4 md:grid md:grid-cols-2">
             <el-form-item label="First Name:" class="form-input-box">
-              <el-input v-model="userData.firstName" required />
+              <el-input v-model="orderData.firstName" required />
             </el-form-item>
             <el-form-item label="Last Name:" class="form-input-box">
-              <el-input v-model="userData.lastName" required />
+              <el-input v-model="orderData.lastName" required />
             </el-form-item>
             <el-form-item label="E-mail:" class="form-input-box">
-              <el-input v-model="userData.email" type="email" required />
+              <el-input v-model="orderData.email" type="email" required />
             </el-form-item>
             <el-form-item label="Phone:" class="form-input-box">
-              <el-input v-model="userData.phone" type="tel" required />
+              <el-input v-model="orderData.phone" type="tel" required />
             </el-form-item>
           </div>
         </section>
-        <section class="my-6">
+        <section class="my-6 md:my-12">
           <AppHeadline :headlineType="HeadlinesTypes.H3">Address:</AppHeadline>
           <div class="flex flex-col gap-4 md:grid md:grid-cols-2">
             <el-form-item label="Street Address:" class="form-input-box">
-              <el-input v-model="userData.address" required />
+              <el-input v-model="orderData.address" required />
             </el-form-item>
             <el-form-item label="City:" class="form-input-box">
-              <el-input v-model="userData.city" required />
+              <el-input v-model="orderData.city" required />
             </el-form-item>
             <el-form-item label="ZIP code:" class="form-input-box">
-              <el-input v-model="userData.zip" required />
+              <el-input v-model="orderData.zip" required />
             </el-form-item>
           </div>
+        </section>
+        <section class="my-6 md:my-12">
+          <AppHeadline :headlineType="HeadlinesTypes.H3"
+            >Select Shipping Method</AppHeadline
+          >
+          <el-radio-group v-model="orderData.shipping">
+            <el-radio value="10" size="large" class="bg-white" border
+              >Standard Shipping 10zł</el-radio
+            >
+            <el-radio value="25" size="large" class="bg-white" border
+              >Express Shipping 25zł</el-radio
+            >
+          </el-radio-group>
+        </section>
+        <section class="my-6 md:my-12">
+          <AppHeadline :headlineType="HeadlinesTypes.H3"
+            >Payment Options</AppHeadline
+          >
+          <el-radio-group v-model="orderData.paymentOption">
+            <el-radio value="card" size="large" class="bg-white" border>
+              <Icon
+                name="solar:card-bold"
+                width="24"
+                height="24"
+                class="text-gray-600 mr-2"
+              />Credit Card
+            </el-radio>
+            <el-radio value="pay-pal" size="large" class="bg-white" border>
+              <Icon
+                name="ic:baseline-paypal"
+                width="24"
+                height="24"
+                class="text-gray-600 mr-2"
+              />PayPal</el-radio
+            >
+            <el-radio value="cash" size="large" class="bg-white" border>
+              <Icon
+                name="vaadin:cash"
+                width="24"
+                height="24"
+                class="text-gray-600 mr-2"
+              />Cash</el-radio
+            >
+          </el-radio-group>
+        </section>
+        <section class="my-6 md:my-12">
+          <AppHeadline :headlineType="HeadlinesTypes.H3"
+            >Order Note (Optional)</AppHeadline
+          >
+          <el-input
+            v-model="orderData.orderNote"
+            maxlength="150"
+            placeholder="Your note..."
+            show-word-limit
+            type="textarea"
+          />
         </section>
       </form>
     </div>
@@ -48,7 +110,7 @@
 <script setup lang="ts">
 import { HeadlinesTypes } from "@/enums/enums";
 
-const userData = reactive({
+const orderData = reactive({
   email: null,
   firstName: null,
   lastName: null,
@@ -56,12 +118,15 @@ const userData = reactive({
   zip: null,
   address: null,
   phone: null,
+  shipping: "10",
+  paymentOption: "card",
+  orderNote: "",
 });
 
 const isLoggedIn = computed(() => {
   const isLoggedIn = window.localStorage.getItem("jwt");
   return isLoggedIn;
-})
+});
 
 function handleSubmitFinishOrder() {
   console.log("Finist order");
@@ -69,7 +134,7 @@ function handleSubmitFinishOrder() {
 
 async function fetchUserData() {
   try {
-    if(!isLoggedIn) return;
+    if (!isLoggedIn) return;
 
     const loggedUserJSONData = window.localStorage.getItem("userData");
 
@@ -79,9 +144,7 @@ async function fetchUserData() {
 
       console.log(data);
     }
-  } catch(err) {
-
-  }
+  } catch (err) {}
 }
 
 await fetchUserData();
