8ee37f6f2511a75d283d6d87e6ad2a2855221fa4
Kacper Nalepa <kacper.nlp@wp.pl>
2024-05-23 22:50:06 +0200

    Display product category and link it on PDP page

--- client/pages/product/[id].vue
+++ client/pages/product/[id].vue
@@ -4,13 +4,22 @@
     <AppSectionBox class="!pb-0">
       <AppBreadcrumbs />
     </AppSectionBox>
-    <AppSectionBox class="grid grid-cols-1 md:grid-cols-2 gap-4 xl:gap-8 2xl:gap-16">
+    <AppSectionBox
+      class="grid grid-cols-1 md:grid-cols-2 gap-4 xl:gap-8 2xl:gap-16"
+    >
       <div class="relative">
-        <AppDiscountPercentTile :price="product.price" :discount="product.discount"
-          class="absolute top-2 md:top-4 right-2 md:right-4 z-20 discount-tile" />
+        <AppDiscountPercentTile
+          :price="product.price"
+          :discount="product.discount"
+          class="absolute top-2 md:top-4 right-2 md:right-4 z-20 discount-tile"
+        />
         <Splide :options="{ rewind: true }">
           <SplideSlide v-for="(img, id) in product.images.data" :key="id">
-            <img :src="$imgUrl(img.attributes.url)" :alt="product.name" class="w-full" />
+            <img
+              :src="$imgUrl(img.attributes.url)"
+              :alt="product.name"
+              class="w-full"
+            />
           </SplideSlide>
         </Splide>
       </div>
@@ -18,21 +27,24 @@
         <div class="flex justify-between max-w-lg mt-2">
           <div>
             <h1 class="mb-2 text-2xl">{{ product.name }}</h1>
-            <AppReviewStars v-if="!!product.reviews" :numberOfReviews="product.reviews.numberOfReviews"
-              :review="product.reviews.review" />
+            <AppReviewStars
+              v-if="!!product.reviews"
+              :numberOfReviews="product.reviews.numberOfReviews"
+              :review="product.reviews.review"
+            />
           </div>
           <div class="text-lg">
             <div v-if="product.discount">
               <span class="text-gray-400 line-through font-normal mr-2">{{
-    $currency(product.price)
-  }}</span>
+                $currency(product.price)
+              }}</span>
               <span class="font-semibold">{{
-      $currency(product.discount)
-    }}</span>
+                $currency(product.discount)
+              }}</span>
             </div>
             <span v-else class="font-semibold">{{
-      $currency(product.price)
-    }}</span>
+              $currency(product.price)
+            }}</span>
           </div>
         </div>
         <div class="flex flex-col gap-2 my-6 text-sm font-light">
@@ -42,16 +54,26 @@
             <span v-else class="font-light text-rose-400">Out of stock</span>
           </div>
           <div>
-            <span class="text-gray-400 font-light">SKU: </span><span>{{ product.sku }}</span>
+            <span class="text-gray-400 font-light">SKU: </span
+            ><span>{{ product.sku }}</span>
           </div>
         </div>
         <p class="max-w-lg font-light">{{ product.shortDesc }}</p>
         <div class="flex flex-col gap-2 my-6 py-8 border-y">
           <div class="flex flex-col gap-1">
-            <label for="productStock" class="mb-2 text-xs">Number of products:</label>
+            <label for="productStock" class="mb-2 text-xs"
+              >Number of products:</label
+            >
             <div class="flex gap-3">
-              <input type="number" name="productStock" id="productStock" min="1" :max="product.stock" v-model="quantity"
-                class="px-4 py-2 rounded-lg border-2" />
+              <input
+                type="number"
+                name="productStock"
+                id="productStock"
+                min="1"
+                :max="product.stock"
+                v-model="quantity"
+                class="px-4 py-2 rounded-lg border-2"
+              />
               <AppButton @click="handleClickAddProductToCart">
                 Add to cart
               </AppButton>
@@ -59,7 +81,10 @@
           </div>
           <span class="my-2 text-sm">
             <span class="text-gray-400 font-light">Category: </span>
-            <NuxtLink to="/category/t-shirt">T-shirt</NuxtLink>
+            <NuxtLink
+              :to="`/category/${product.category.data.attributes.key}`"
+              >{{ product.category.data.attributes.name }}</NuxtLink
+            >
           </span>
         </div>
         <div class="flex flex-col sm:flex-row gap-4 text-sm text-gray-400">
@@ -67,8 +92,15 @@
             <Icon name="ion:heart-outline" width="18" height="18" />
             <span class="ml-2">Add to wishlist</span>
           </button>
-          <button @click="handleClickShareProduct" class="flex hover:text-indigo-600">
-            <Icon name="material-symbols-light:share-outline" width="18" height="18" />
+          <button
+            @click="handleClickShareProduct"
+            class="flex hover:text-indigo-600"
+          >
+            <Icon
+              name="material-symbols-light:share-outline"
+              width="18"
+              height="18"
+            />
             <span class="ml-2">Share</span>
           </button>
         </div>
@@ -87,7 +119,11 @@
       </el-tabs>
     </AppSectionBox>
     <AppSectionBox class="mb-2 md:mb-8 xl:mb-16">
-      <AppHeadline :headlineType="HeadlinesTypes.H2" class="uppercase text-center">Can interested you:</AppHeadline>
+      <AppHeadline
+        :headlineType="HeadlinesTypes.H2"
+        class="uppercase text-center"
+        >Can interested you:</AppHeadline
+      >
       <!-- <AppProductsSlider /> -->
     </AppSectionBox>
   </div>
@@ -108,20 +144,20 @@ const quantity = ref(1);
 const activeName = ref("first");
 const isLoading = ref(true);
 const product = reactive<ProductAttributes>({
-  name: '',
-  shortDesc: '',
-  description: '',
+  name: "",
+  shortDesc: "",
+  description: "",
   price: 0,
   discount: null,
   images: {
-    data: null
+    data: null,
   },
   isNew: false,
   reviews: null,
   category: {
-    data: null
+    data: null,
   },
-  sku: '',
+  sku: "",
   stock: 0,
 });
 
@@ -142,7 +178,9 @@ function handleClickShareProduct() {
 function handleClickAddProductToCart() {
   const { name, price, discount, images, reviews } = product;
 
-  const thumbnail = Array.isArray(images.data) ? images.data[0].attributes.url : 'placeholder-url';
+  const thumbnail = Array.isArray(images.data)
+    ? images.data[0].attributes.url
+    : "placeholder-url";
 
   const addedProduct = {
     id: route.params.id as string,
