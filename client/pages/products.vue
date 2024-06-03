2df44c4cf2f1044aa36302737b43758a19872f08
Kacper Nalepa <kacper.nlp@wp.pl>
2024-06-04 00:29:22 +0200

    Add clear filters button and condition to display this button

--- client/pages/products.vue
+++ client/pages/products.vue
@@ -7,20 +7,36 @@
         </el-collapse-item>
         <el-collapse-item title="Categories" name="2">
           <el-checkbox-group v-model="filters.checkedCategories">
-            <el-checkbox v-for="{ attributes, id } in categories" :label="attributes.name" :value="attributes.key"
-              :key="id" />
+            <el-checkbox
+              v-for="{ attributes, id } in categories"
+              :label="attributes.name"
+              :value="attributes.key"
+              :key="id"
+            />
           </el-checkbox-group>
         </el-collapse-item>
         <el-collapse-item title="Sales Products Only" name="3">
           <el-checkbox v-model="filters.isSaleOnly" />
         </el-collapse-item>
       </el-collapse>
+      <el-button class="mt-4" type="primary">Filtruj</el-button>
+      <div class="mt-2" v-if="isClearFilterBtnVisible">
+        <el-button @click="clearFilters" type="danger"
+          >Wyczyść filtry</el-button
+        >
+      </div>
     </div>
     <div class="w-full">
       <ul class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8">
         <li v-for="{ id, attributes } in products" :ke="id">
-          <AppProductBox :id="id" :name="attributes.name" :price="attributes.price" :discount="attributes.discount"
-            :imgs="attributes.images.data" :reviews="attributes.reviews" />
+          <AppProductBox
+            :id="id"
+            :name="attributes.name"
+            :price="attributes.price"
+            :discount="attributes.discount"
+            :imgs="attributes.images.data"
+            :reviews="attributes.reviews"
+          />
         </li>
       </ul>
     </div>
@@ -60,13 +76,15 @@ const filters = reactive({
 //   return productsCopy;
 // });
 
+const isClearFilterBtnVisible = computed(() => {
+  return filters.checkedCategories.length || filters.isSaleOnly;
+});
+
 async function getLoadData() {
   try {
     const [categoriesDataRespons, productsDataResponse] = await Promise.all([
       useAPIFetch("/categories"),
-      useAPIFetch(
-        `/products?populate=*`
-      ),
+      useAPIFetch(`/products?populate=*`),
     ]);
 
     categories.value = categoriesDataRespons;
@@ -76,12 +94,16 @@ async function getLoadData() {
   }
 }
 
+function clearFilters() {
+  filters.isSaleOnly = false;
+  filters.checkedCategories.length = 0;
+}
+
 await getLoadData();
 </script>
 
 <style lang="scss">
 .el-collapse-item {
-
   &__header,
   &__content {
     background: #f3f4f6;
